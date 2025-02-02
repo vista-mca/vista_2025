5690cd97fcdc89272754e968ccaecb2df1c0e7fc
Omkar Prajapati <omkar.devx@gmail.com>
2025-02-02 22:27:17 +0530

    hero height fixed

--- src/components/HomeComponents/Hero.jsx
+++ src/components/HomeComponents/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         muted
         loop
         playsInline
-        className="w-full md:h-[38rem] md:object-cover"
+        className="w-full md:h-screen  md:object-cover"
       >
         <source src={heroVideo} type="video/webm" />
         <p>
